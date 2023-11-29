b7c21ccf89cf21804034aa13939d496fc484d6b3
Jarrod <gatejarr@gmail.com>
2023-11-28 20:57:02 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import NotesList from './components'
 </script>
 
 <template>
   <div>
     <h1>Vue Notes</h1>
+    <NotesList />
     <!-- your code here -->
   </div>
 </template>
