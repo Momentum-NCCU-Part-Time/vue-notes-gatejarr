ae5ac53677e6182e0881508b4423830127951fa8
Jarrod <gatejarr@gmail.com>
2023-12-05 17:09:48 -0500

    WIP; API requests done, need to pull into NoteList.vue and create buttons for new, edit, delete.Date also not showing correctly on sample note. Entire project has no formating.

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,3 +1,14 @@
+<template>
+  <ul>
+    <li v-for="note in notes"
+      :key="note.id">
+      {{ note.title }},
+      {{ note.body }},
+      {{ note.updatedAt }}
+    </li>
+  </ul>
+</template>
+
 <script setup>
 import { ref } from 'vue'
 import { getNotes } from '@/requests'
@@ -7,13 +18,3 @@ getNotes().then((data) =>{
   notes.value = data
 })
 </script>
-
-<template>
-  <ul>
-    <li v-for="note in notes"
-      :key="note.id">
-      {{ note.title }},
-      {{ note.body }}
-    </li>
-  </ul>
-</template>
--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,33 @@
 const apiURL = "http://localhost:3000/notes";
+
 export const getNotes = () => {
   return fetch(apiURL).then((response) => response.json());
+};
+
+export const createNote = (note) => {
+  const { title, body } = note;
+  return fetch(apiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, body, createdAt: new Date() }),
+  }).then((response) => response.json());
+};
+
+export const editNote = (note) => {
+  const { id, title, body } = note;
+  return fetch("${apiURL}/${id}", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, body }),
+  }).then((response) => response.json());
+};
+
+export const deleteNote = (note) => {
+  return fetch("${apiURL}/${id}", {
+    method: "DELETE",
+  }).then((response) => response.json());
 };
